b2cf67d4b8f282018e0ffdb834f99343b957a9af
Rouan van der Ende <rouan@8bo.org>
2024-06-04 10:07:42 +0200

    0.1.34 eslint error fix

--- src/NSurreal.ts
+++ src/NSurreal.ts
@@ -185,7 +185,10 @@ export class NSurreal<G extends Record<string, object>> {
       await fs.promises
         .writeFile(
           `${this.output_path}/querytypes/${uid}.ts`,
-          `import type { UUID, RecordId } from "surrealdb.js";
+          `/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+          
+          import type { UUID, RecordId } from "surrealdb.js";
 export ${outputtype.join("\n")}`
         )
         .catch((err) => {
